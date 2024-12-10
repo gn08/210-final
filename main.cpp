da9eb6725e15d914d388856eb61077cc61fd5282
gn08 <gabbyfilipova@gmail.com>
2024-12-10 12:11:27 -0800

    commit 12:10

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct CoffeeCustomer {
+    string name;
+    string drink_order;
+    CoffeeCustomer * next;
+}
+
+class CoffeeQueue{
+
+};
+
+int main(){
+
+    return 0;
+}
